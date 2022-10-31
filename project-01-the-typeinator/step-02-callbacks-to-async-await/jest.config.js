d635a42dc3170564ddaf59abf43816682217c711
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2022-10-31 21:53:09 +0500

    initialize project 01

---
+++ project-01-the-typeinator/step-02-callbacks-to-async-await/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	fakeTimers: { enableGlobally: true },
+	// transform: {
+	// 	"^.+\\.(t|j)sx?$": [
+	// 		"@swc/jest",
+	// 		{
+	// 			jsc: {
+	// 				target: "es2021",
+	// 			},
+	// 		},
+	// 	],
+	// },
+};
